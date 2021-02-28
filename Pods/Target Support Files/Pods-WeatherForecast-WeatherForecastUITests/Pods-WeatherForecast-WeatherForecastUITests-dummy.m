030f599032726c6198caf27120ebf1c1bbc39301
httruc <hoangthuytruc@gmail.com>
2021-02-28 21:53:20 +0700

    ignore Pods

--- Pods/Target Support Files/Pods-WeatherForecast-WeatherForecastUITests/Pods-WeatherForecast-WeatherForecastUITests-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_WeatherForecast_WeatherForecastUITests : NSObject
-@end
-@implementation PodsDummy_Pods_WeatherForecast_WeatherForecastUITests
-@end
